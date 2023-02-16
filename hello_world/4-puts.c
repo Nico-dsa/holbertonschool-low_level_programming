00bec43894d5b6ed47bb54c45b59ea0ba5d37006
Nico-dsa <6463@holbertonstudents.com>
2023-02-16 07:58:46 -0800

    Task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry
- * Return : 0 (Sucess)
+ * Return : Always 0 (Sucess)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0)
+	return (0);
 }
