32e7152f3222ba4eb26447f13c1249bb9fc002d9
Nico-dsa <6463@holbertonstudents.com>
2023-02-21 00:48:24 -0800

    Task 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num > 16; num--)
+		puthcar ((num % 10) + '0')
+
+	putchar ('\n')
+
+	Return : (0);
+}
