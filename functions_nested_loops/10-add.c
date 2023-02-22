99ef47f57649eb28fd09b030dfd26da3c056a170
Nico-dsa <6463@holbertonstudents.com>
2023-02-22 06:48:28 -0800

    Task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * add - a function that adds two integers and returns the result
+ * @a: first number
+ * @b: second number
+ *
+ * Return: integer value
+ */
+
+int add(int a, int b)
+{
+	return (a + b)
+}
---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * time_table - prints the 9 times table, starting with 0
+ * :
