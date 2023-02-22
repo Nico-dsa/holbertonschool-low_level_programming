eb7c5c84a53a295002100d6ec32fac69d2ea46b5
Nico-dsa <6463@holbertonstudents.com>
2023-02-22 03:32:37 -0800

    Task 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ * Return: Always 0
+ */
+
+void jack_bauer(void)
+{
+	int a, b;
+
+	for (a = 0; a < 24; a++)
+	{
+		for (b = 0, b < 60, b++)
+		{
+			_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+			_putchar(':');
+			_putchar((b / 10) + '0');
+			_putchar((b / 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
