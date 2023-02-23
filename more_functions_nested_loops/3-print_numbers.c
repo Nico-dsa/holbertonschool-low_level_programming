b67584b3657ef6aa8a0dc5a7300981bc001b9bf9
Nico-dsa <6463@holbertonstudents.com>
2023-02-23 04:35:30 -0800

    Task 3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -12,10 +12,10 @@
 
 void print_numbers(void)
 {
-char i;
-for (i = '0'; i <= '9'; i++)
-{
-_putchar(i);
-}
-_putchar('\n');
+	char i;
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
 }
