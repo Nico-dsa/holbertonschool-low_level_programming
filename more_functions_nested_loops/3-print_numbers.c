f98ad47381b8231f5f86c82ed2c578c9de5f600d
Nico-dsa <6463@holbertonstudents.com>
2023-02-23 04:30:48 -0800

    Task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * print_numbers - function that prints the numbers, from 0 to 9, followed by a new line
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+
+void print_numbers(void)
+{
+char i;
+for (i = '0'; i <= '9'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
