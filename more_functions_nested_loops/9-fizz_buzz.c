965b7fa55149d578760a56a050a3721ff812b07e
Nico-dsa <6463@holbertonstudents.com>
2023-02-23 06:51:35 -0800

    Task 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -14,13 +14,13 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i%15 == 0)
-			printf ("FizzBuzz\n");
-		else if (i%3 == 0)
-		       printf ("Fizz\n");
-		else if (i%5 == 0)
-			printf ("Buzz\n");
+			printf ("FizzBuzz");
+		else if ((i%3) == 0)
+		       printf ("Fizz");
+		else if ((i%5) == 0)
+			printf ("Buzz");
 		else
-			printf ("%d\n", i);
+			printf ("%d", i);
 	}
 	return 0;
 }
