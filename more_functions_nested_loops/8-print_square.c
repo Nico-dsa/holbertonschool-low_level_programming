b69c8ce6df378f9f2aa5a129009833ad160bdbfb
Nico-dsa <6463@holbertonstudents.com>
2023-02-23 05:59:43 -0800

    Task 8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -19,10 +19,10 @@ void print_square(int size)
 	}
 	else
 	{
-		for (i = 0; i < size; i++)
+		for (i = 0; i <= size; i++)
 		{
 			_putchar('#');
-			for (j = 0; j < size; j++)
+			for (j = 0; j <= size; j++)
 			{
 				_putchar('#');
 			}
