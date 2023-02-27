d12d75aef222735e4ec882fd7a54565106efda27
Nico-dsa <6463@holbertonstudents.com>
2023-02-27 05:53:49 -0800

    Task 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - function that reverses a string
+ * @s: String to renverse
+ *
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	char a = s[0];
+	int c = 0;
+	int i;
+
+	while (s[c] != '\0')
+		c++;
+
+	for (i = 0; i < c; i++)
+	{
+		c--;
+		a = s[i];
+		s[i] = s[c];
+		s[c] = a;
+	}
+}
