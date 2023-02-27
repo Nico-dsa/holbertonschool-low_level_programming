7d123afeb0565ac527eaa05897f48125202af92c
Nico-dsa <6463@holbertonstudents.com>
2023-02-27 01:12:54 -0800

    Task 0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 /**
- * reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98
- * @n - pointer to n
+ * reset_to_98 - takes a pointer to an int as parameter and updates the value
+ * @n: pointer to n
  *
  * Return: 0
  */
