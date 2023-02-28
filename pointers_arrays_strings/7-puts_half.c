e9191a1a104a6a4da215fdc50fd6797e6bffd373
Nico-dsa <6463@holbertonstudents.com>
2023-02-28 01:21:04 -0800

    Task 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -3,6 +3,7 @@
 
 /**
  * puts_half - function that prints half of a string, followed by a new line
+ * if odd len, n = (length_of_the_string - 1) / 2
  * @str: input
  *
  * Return: 0
@@ -10,19 +11,19 @@
 
 void puts_half(char *str)
 {
-	int i; j; len;
+	int i, n, len;
 
 	len = 0;
 
 	for (i = 0; str[i] != '\0'; i++)
 		len++;
 
-	j = (len / 2);
+	n = (len / 2);
 
 	if ((len % 2) == 1)
-		j = ((len + 1) / 2);
+		n = ((len + 1) / 2);
 
-	for (i = j; str[i] != '\0'; i++)
+	for (i = n; str[i] != '\0'; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
