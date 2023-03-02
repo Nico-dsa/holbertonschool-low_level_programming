85a7a18ea4810c94b8355975c89e7505599fe5da
Nico-dsa <6463@holbertonstudents.com>
2023-03-02 01:45:03 -0800

    P3 Task 0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * _memset - unction that fills memory with a constant byte.
+ * @s: menory area
+ * @b: constant byte
+ * @n: byte of memory area
+ *
+ * Return: 0
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+	for (a = 0; a < n; a++)
+	{
+		s[a] = b;
+	}
+	return (s);
+}
