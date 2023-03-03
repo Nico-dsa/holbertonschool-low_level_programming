c6ab63fc244d401692f57a55b4d49abb5e3e04a2
Nico-dsa <6463@holbertonstudents.com>
2023-03-03 03:38:22 -0800

    P3 Task 6

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * _strstr - function that locates a substring.
+ * @haystack: string to search
+ * @needle: string to find
+ *
+ * Return: 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
