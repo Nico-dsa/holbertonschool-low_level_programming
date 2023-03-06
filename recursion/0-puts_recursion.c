6d403838f90a2ad67cb2c6466bc574ef159b84f0
Nico-dsa <6463@holbertonstudents.com>
2023-03-06 01:43:35 -0800

    Task 0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string, followed by a new line
+ * @s: string
+ *
+ * Return: 0
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
