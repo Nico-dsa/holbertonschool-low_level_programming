914158b3ab2dd079183813079ca930bc52e83f48
Nico-dsa <6463@holbertonstudents.com>
2023-03-08 03:19:53 -0800

    Task 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -14,8 +14,8 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		n1 = atoi(argc[1]);
-		n2 = atoi(argc[2]);
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
 		printf("%d\n", n1 * n2);
 	}
 	else
