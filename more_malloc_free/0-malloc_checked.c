56ef62ebe291489222b143d2ae42aabda5b6500b
Nico-dsa <6463@holbertonstudents.com>
2023-03-15 02:42:18 -0700

    Task 0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - funtion that allocates memory using malloc
+ * @b: the size to allocate
+ *
+ * Return: 0
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}
