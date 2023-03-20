f8bcc6182b5347f2bda23eb4c28b40f4d83d4f99
Nico-dsa <6463@holbertonstudents.com>
2023-03-20 02:44:54 -0700

    Task 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog attributes
+ * @name: name dog
+ * @age: age dog
+ * @owner: owner dog
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
