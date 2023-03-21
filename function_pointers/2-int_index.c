aac5e88ace3d140f9b2a36ac1c3559c3beb1e19b
Nico-dsa <6463@holbertonstudents.com>
2023-03-21 03:22:22 -0700

    Task 2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - function that searches for an integer
+ * @array: array
+ * @size: number of elements in the array
+ * @cmp: pointer to the function
+ *
+ * Return: 0
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			while (i < size)
+			{
+				if (cmp(array[i]))
+					return (i);
+				i++;
+			}
+		}
+	}
+	return (-1);
+}
