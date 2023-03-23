e8a798dd93772f53859454e7d1739d1bfb301599
Nico-dsa <6463@holbertonstudents.com>
2023-03-23 02:50:04 -0700

    Task 3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -11,7 +11,7 @@
 
 int (*get_op_func(char *s))(int, int)
 {
-	 op_t ops[] = {
+         op_t ops[] = {
         {"+", op_add},
         {"-", op_sub},
         {"*", op_mul},
@@ -24,9 +24,9 @@ int (*get_op_func(char *s))(int, int)
 
     while (i < 5)
     {
-	    if (ops[i].op[0] == s[0])
-		    return (ops[i].f);
-	    i++;
+            if (ops[i].op[0] == s[0])
+                    return (ops[i].f);
+            i++;
     }
     return (NULL);
 }
