3b8df94460ba3ce3c43f604f40a4a490316a4fb5
Nico-dsa <6463@holbertonstudents.com>
2023-04-06 07:48:18 -0700

    Task 4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+  * free_dlistint - function that frees a dlistint_t list.
+  * @head: head of the linked list
+  * 
+  * Return: 0
+  */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
