7b115f5132df562a0eb9cf2839c185dc4bd54f7f
Nico-dsa <6463@holbertonstudents.com>
2023-04-11 01:49:25 -0700

    Task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
@@ -29,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 	if (wr == -1)
 		return (-1);
 
-	close (fd);
+	close(fd);
 
 	return (1);
 }
