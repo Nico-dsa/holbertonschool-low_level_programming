fda5e8d893511865538bb5dc7fc2420aaca37f3b
Nico-dsa <6463@holbertonstudents.com>
2023-04-11 01:17:34 -0700

    Task 1

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * read_textfile - function that reads a text file and prints
+ * @filename: name of the file
+ * @letters: numbers of letters printed
+ *
+ * Return: the actual number of letters it could read and print, if fails, return 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t rd, wr;
+	char *buf;
+
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	rd = read(fd, buf, letters);
+	wr = write(STDOUT_FILENO, buf, rd);
+
+	close (fd);
+
+	free(buf);
+
+	return (wr);
+}
