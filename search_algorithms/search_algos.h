012f156153b292c601ee6fdbb0508757f252ab80
Nico-dsa <6463@holbertonstudents.com>
2023-05-25 05:35:04 -0700

    function that searches for a value in an array of integers

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef ALGOS
+#define ALGOS
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
